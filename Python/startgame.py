a2bf22a62048f10386dbb3180670190d5ad0a59a
Yanni (sandwich) <spock@scail.mit.edu>
2014-01-16 20:04:52 -0500

    added map transmission support as well as a sample map

---
+++ Python/startgame.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python
+import socket, sys, time, base64, json
+
+HOST=sys.argv[1]
+PORT=sys.argv[2]
+mapfile=sys.argv[3]
+map=open(mapfile, 'r').read()[:-1]
+map_cmd='{"token":"666","MAP":"'+map+'"}done\n'
+game_cmd='{"token":"666","GAME":"start"}done\n'
+
+s=socket.socket(socket.AF_INET, socket.SOCK_STREAM);
+s.connect((HOST, int(PORT)))
+s.sendall(map_cmd)
+time.sleep(0.5)
+s.sendall(game_cmd)
+s.close()
